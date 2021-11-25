2fdcc8d505ea00ced16aab2f8fb707382b02ace9
vishalgautam1 <gvishal630@gmail.com>
2021-11-25 16:31:44 +0530

    add first 5 exercise

---
+++ Assignment/assign1.py
@@ -0,0 +1,25 @@
+#Assignment 1 - 'Comments using #', "Calculator", "Git Initialization", "Atom - Shorcuts",
+#Calculator
+#Inch to cm conversion
+print("inch to cm")
+inches = int(input("Enter the length in inches = "))
+cm = 2.54*inches
+print("The length in cm", cm)
+
+#Pounds to Kg conversion
+print("Pounds to Kg")
+Pounds = int(input("Enter the weight in pounds = "))
+Kg = 0.453592*Pounds
+print("The weight in Kg", Kg)
+
+#Git Initialization
+# Bottom Right corner = Select "Git"
+# Create Repository
+# Inilize git repository in directory - path / location =
+# Git Identity
+
+#print("Atom Shortcuts")
+#print("Delete to End -ctrl+shift+K")
+#print("Select Line - ctrl L")
+#print("git tab - ctrl + Shift + 9")\
+#print("Github tab - ctrl + shift + 8")
---
+++ ex5.py
@@ -0,0 +1,17 @@
+name = 'Zed A. Shaw'
+age = 35 #not a lie
+my_height = 74 #inches
+my_weight = 180 #lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print("Let's talk about %r." %name)
+print("He's %r inches tall." %my_height)
+print("He's %d pounds heavy." %my_weight)
+print("Actually that's not too heavy.")
+print("He's got %s eyes and %s hair."%(my_eyes, my_hair))
+print("His teeth are usually %s depending on the coffe." %my_teeth)
+
+#this line is tricky, try to get it exactly right 
+print("If I add %d, %d and %d I get %d." %(age, my_height, my_weight, age+my_height+my_weight))
